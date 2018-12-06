8f9b51f5d0e954cf6ac31c33dfae248fe95029a0
Brian McClain <brianmmcclain@gmail.com>
2018-12-05 23:17:48 -0500

    Testing

--- src/main/java/com/github/brianmmcclain/uppercase/UppercaseApplication.java
+++ src/main/java/com/github/brianmmcclain/uppercase/UppercaseApplication.java
@@ -11,7 +11,9 @@ public class UppercaseApplication {
 
 	@Bean
     public Function<String, String> uppercase() {
-        return s -> s.toUpperCase();
+		String pgUser = System.getProperty("PG_USER", "postgres");
+		String pgPass = System.getProperty("PG_PASS", "defaultPass");
+        return s -> s.toUpperCase() + " -- " + pgUser + "/" + pgPass;
     }
 	public static void main(String[] args) {
 		SpringApplication.run(UppercaseApplication.class, args);
